e8f5ada84414f53c2d2c718f86803e32eeae018f
Brett Patterson <brett.patterson@classyllama.com>
2022-03-06 21:46:31 -0500

    Update initialization

--- gitman_init.sh
+++ gitman_init.sh
@@ -37,13 +37,21 @@ echo "Cleaned Project ID: '${ProjectID}'"
 # Link from persistent directory to source
 [[ -L persistent/source ]] || ln -s ${SOURCE_DIR_FROM_PERSIST_DIR} persistent/source
 
+# Create symlinks for things that don't change each project
+[[ -L persisent/database ]] || ln -s database persistent/database
+[[ -L persisent/elasticsearch ]] || ln -s elasticsearch persistent/elasticsearch
+
+# Copy sample configuration directories if they do not exist yet.
+[[ -d persisent/nginx ]] || cp -R nginx persisent/
+[[ -d persisent/secrets ]] || mkdir persisent/secrets
+[[ -d persisent/varnish ]] || cp -R varnish persisent/
+
 # Copy sample files to persistent directory if they do not exist yet.
 [[ -f persistent/.gitignore ]] || cp .gitignore.sample persistent/.gitignore
 [[ -f persistent/docker-compose.yml ]] || cp docker-compose.yml persistent/docker-compose.yml
 [[ -f persistent/.env ]] || awk -v prjid="${ProjectID}" '{gsub(/{{ID}}/,prjid,$0); print $0}' templates/.env > persistent/.env
 [[ -f persistent/mutagen.yml ]] || awk -v prjid="${ProjectID}" '{gsub(/{{ID}}/,prjid,$0); print $0}' templates/mutagen.yml > persistent/mutagen.yml
-[[ -d persisent/database ]] || cp -R database persisent/
-[[ -d persisent/elasticsearch ]] || cp -R elasticsearch persisent/
-[[ -d persisent/nginx ]] || cp -R nginx persisent/
-[[ -d persisent/secrets ]] || mkdir persisent/secrets
-[[ -d persisent/varnish ]] || cp -R varnish persisent/
+
+# Generate database passwords if they don't exist
+[[ -f persisent/secrets/mariadb.root.secret ]] || cat /dev/urandom | LC_CTYPE=C tr -dc '[:alnum:][:punct:]' | fold -w ${1:-32} | head -n 1 > mariadb.root.secret
+[[ -f persisent/secrets/mariadb.user.secret ]] || cat /dev/urandom | LC_CTYPE=C tr -dc '[:alnum:][:punct:]' | fold -w ${1:-16} | head -n 1 > mariadb.user.secret
